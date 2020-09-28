d7914aad1d903d55942c23937199f6bd8c6a1232
vagrant <vagrant@localhost.localdomain>
2020-09-28 18:31:22 +0800

    add

--- console/migrations/m200921_092431_create_table_title.php
+++ console/migrations/m200921_092431_create_table_title.php
@@ -8,23 +8,23 @@ use Faker\Factory;
  */
 class m200921_092431_create_table_title extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-		$this->createTable('title',[
-			'id'=>$this->primaryKey(),
-			'title'=>$this->string(),
-			'author'=>$this->string(),
-			'date'=>$this->date(),
-			'text'=>$this->text(),
-			'create_time'=>$this->time(),
-			'status'=>$this->integer(),
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp()
+	{
+		$this->createTable('title', [
+			'id'          => $this->primaryKey(),
+			'title'       => $this->string(),
+			'author'      => $this->string(),
+			'date'        => $this->date(),
+			'text'        => $this->text(),
+			'create_time' => $this->time(),
+			'status'      => $this->integer(),
 		]);
 		
-		$faker=Factory::create();
-		$column=[
+		$faker = Factory::create();
+		$column = [
 			'title',
 			'author',
 			'date',
@@ -32,43 +32,47 @@ class m200921_092431_create_table_title extends Migration
 			'create_time',
 			'status'
 		];
-	
-		$arr=[];
-		$time=10;
-		for($i=0;$i<$time;$i++){
-			$arr[]=[
+		
+		$arr = [];
+		$time = 10;
+		for ($i = 0; $i < $time; $i++) {
+			$arr[] = [
 				$faker->sentence(),
 				$faker->name(),
 				$faker->date(),
 				$faker->text(),
 				$faker->time(),
-				$faker->randomElement([1,2,3])
+				$faker->randomElement([
+					1,
+					2,
+					3
+				])
 			];
 		}
 		
-		$this->batchInsert('title',$column,$arr);
-    }
+		$this->batchInsert('title', $column, $arr);
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown()
+	{
+		$this->dropTable('title');
+	}
+	
+	/*
+	// Use up()/down() to run migration code without a transaction.
+	public function up()
+	{
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        $this->dropTable('title');
-    }
+	}
 
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
+	public function down()
+	{
+		echo "m200921_092431_create_table_title cannot be reverted.\n";
 
-    }
-
-    public function down()
-    {
-        echo "m200921_092431_create_table_title cannot be reverted.\n";
-
-        return false;
-    }
-    */
+		return false;
+	}
+	*/
 }
